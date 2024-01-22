0c530e16157fe317f043a2f299cab37fa9bdaddc
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-01-22 12:13:03 -0300

    fix: boxuk colors

--- hypr/lock/boxuk.sh
+++ hypr/lock/boxuk.sh
@@ -7,9 +7,9 @@ swaylock \
 	--indicator-thickness 7 \
 	--effect-blur 7x5 \
 	--effect-vignette 0.5:0.5 \
-	--ring-color a11c1c \
-	--key-hl-color ffa095 \
-	--line-color 9a413d88 \
+	--ring-color 148b85 \
+	--key-hl-color 045069 \
+	--line-color 259c96 \
 	--inside-color 91919133 \
 	--text-color ffffff \
 	--separator-color 00000000 \
